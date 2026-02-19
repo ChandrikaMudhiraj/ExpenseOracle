6d1b532aee2a21e01986acc0428b2684ba062ae7
ChandrikaMudhiraj <Chandrika5mudhiraj@gmail.com>
2026-02-19 19:49:48 +0530

    feat(user): implement user model, schema, and repository layer

--- app/models/user.py
+++ app/models/user.py
@@ -1,13 +1,14 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
 from app.core.database import Base
+
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    email = Column(String, unique= True, index=True)
-    password = Column(String)
-    hashed_password = Column(String)
-    created_at = Column(String)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    
 
